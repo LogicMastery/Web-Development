3145b8becc3cc0c2758bfbc8e554aea106aff770
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-03-30 19:05:18 +0800

    added console-log.html and console-log.js file

---
+++ Lessons/JavaScript/console-log.js
@@ -0,0 +1,12 @@
+var math = 90;
+var sentence = "I got  in math horray!";
+var introduction = "My name is ";
+var name = "Logic Mastery";
+
+
+sentence = "I got " + math + " in math horray! "
+introduction = "My name is " + name;
+
+console.log(sentence + introduction)
+console.warn("WARNING WARNING BREACH DETECTED")
+console.error("ERROR 404 VIRUS DETECTED")
