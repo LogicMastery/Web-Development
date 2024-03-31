3f98f5de977032e0f9a43fcfb6aad75a93c7f8af
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-03-31 20:47:40 +0800

    added string-manipulation.html and string-manipulation.js

---
+++ Lessons/JavaScript/string-manipulation.js
@@ -0,0 +1,16 @@
+var headingSentence = document.getElementById("random-sentence")
+var randomsentence = headingSentence.innerText
+
+var UpperSentence = randomsentence.toUpperCase()
+var LowerSentence = randomsentence.toLowerCase()
+var firstElevenChars = randomsentence.substr(0, 11)
+
+var AllWords = randomsentence.split(" ")
+var BracketWords = "[" + AllWords.join("][") + "]"
+
+var FirstThreeWords = AllWords.slice(0, 3)
+
+var MiddleWords = AllWords.slice(5, 10)
+var MakeSentence = MiddleWords.join(" ")
+
+headingSentence.innerText = MakeSentence
