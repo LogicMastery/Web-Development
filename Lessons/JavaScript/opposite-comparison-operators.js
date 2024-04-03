71fd0b8028fb0587e0ceac86cb150d2e856a1179
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-03 22:48:13 +0800

    updated a few thing in my opposite-comparison-operators.js file

--- Lessons/JavaScript/opposite-comparison-operators.js
+++ Lessons/JavaScript/opposite-comparison-operators.js
@@ -10,14 +10,14 @@ if(innerLower === "javascript for beginners") {
     text.innerHTML = "Sorry :( YOU ARE IN THE WRONG COURSE. Please try entering another course."
 }
 
-// I made an if and else statement about scoring if totalscore is greater than 79 execute the text if it false excute the else statement it will also execute another text.
+// I made and if and else statement about scoring. If total score is greater than 79, execute the text. If it is false, execute the else statement it will also execute another text.
 var answer = prompt("What's your exam score? ")
 var test1 = document.getElementById("test")
 var totalscore = answer.toLowerCase()
 
-// I made a mistake in the if statement, I type a semicolon instead of curly braces.
+// I made a mistake in the if statement; I typed a semicolon instead of curly braces.
 if(totalscore > 79){
-    test.innerHTML = "<strong>Congratz!</strong> YOU PASSED THE EXAM, your going to graduate this year!"
+    test.innerHTML = "<strong>Congratz!</strong> YOU PASSED THE EXAM, you're going to graduate this year!"
 }else{
     test.innerHTML = "<strong>Sorry :(</strong> YOU FAILED THE EXAM, better luck next time...."
 }
