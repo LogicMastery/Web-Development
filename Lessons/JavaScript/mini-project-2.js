b689fe21395f67baac4d3ab72f9e1d9d37bc6795
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-07 17:08:58 +0800

    updated my mini project #2

--- Lessons/JavaScript/mini-project-2.js
+++ Lessons/JavaScript/mini-project-2.js
@@ -1,9 +1,23 @@
-var url = prompt("Enter a URL: ")
-var link1 = document.getElementById("link")
-var linking = link1.innerHTML
+/* This code explains if it checks if it has an HTTPS, then the if statement will execute, which will tell you that the URL is correct. 
+If not, it will execute the else statement, which is it will tell you that you entered the wrong URL */
+var url1 = prompt("Enter a link: ")
+var Headinglink = document.getElementById("link1")
+var linking = Headinglink.innerHTML
 
-if(url.substr(0, 5) == "https") {
-    link.innerHTML = "Your URL is indeed correct!"
+if(url1.substr(0, 4) == "http" || url1.substr(0, 5) == "https") {
+    link1.innerHTML = "Your URL is indeed correct!"
 }else {
-    link.innerHTML = "Your URL is incorrect, please try again"
+    link1.innerHTML = "Your URL is incorrect, please try again"
 }
+// When we did not add an https:// the computer fixes for us
+var url = prompt("Enter a URL: ")
+
+if(url.substr(0, 5) != "https") {
+    url = "https://" + url
+}
+
+var spanNodelink = document.getElementById("link2")
+var spanNodelinking = spanNodelink.innerHTML
+var HTMLLink = "<a href='" + url + "'>" + url + "</a>";
+spanNodelink.innerHTML = HTMLLink
+var spanNodelinking = spanNodelink.innerHTML
