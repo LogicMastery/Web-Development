0fdf2e5d5a7d77c26372d51f65707a42aace8c2c
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-07 17:15:50 +0800

    updated a few more thing in my mini project #2

--- Lessons/JavaScript/mini-project-2.js
+++ Lessons/JavaScript/mini-project-2.js
@@ -4,20 +4,29 @@ var url1 = prompt("Enter a link: ")
 var Headinglink = document.getElementById("link1")
 var linking = Headinglink.innerHTML
 
-if(url1.substr(0, 4) == "http" || url1.substr(0, 5) == "https") {
-    link1.innerHTML = "Your URL is indeed correct!"
-}else {
-    link1.innerHTML = "Your URL is incorrect, please try again"
+if(url1 == null){
+    console.error("Missing input at the url1")
+}else{
+    if(url1.substr(0, 4) == "http" || url1.substr(0, 5) == "https") {
+        link1.innerHTML = "Your URL is indeed correct!"
+    }else {
+        link1.innerHTML = "Your URL is incorrect, please try again"
+    }
 }
+
 // When we did not add an https:// the computer fixes for us
 var url = prompt("Enter a URL: ")
 
-if(url.substr(0, 5) != "https") {
-    url = "https://" + url
+if(url == null){
+    console.error("Missing input at the url")
+}else{
+    if(url.substr(0, 5) != "https") {
+        url = "https://" + url
+    }
+    
+    var spanNodelink = document.getElementById("link2")
+    var spanNodelinking = spanNodelink.innerHTML
+    var HTMLLink = "<a href='" + url + "'>" + url + "</a>";
+    spanNodelink.innerHTML = HTMLLink
+    var spanNodelinking = spanNodelink.innerHTML
 }
-
-var spanNodelink = document.getElementById("link2")
-var spanNodelinking = spanNodelink.innerHTML
-var HTMLLink = "<a href='" + url + "'>" + url + "</a>";
-spanNodelink.innerHTML = HTMLLink
-var spanNodelinking = spanNodelink.innerHTML
