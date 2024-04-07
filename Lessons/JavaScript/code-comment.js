4b40b938d68a4f4162d047ebb0a82d6a09d6ff36
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-07 22:02:22 +0800

    added code comment both .js and .html file

---
+++ Lessons/JavaScript/code-comment.js
@@ -0,0 +1,14 @@
+var name = "David"
+console.log(name)
+// This is a single line comment
+console.log("Hi, I'm a single line comment")
+/*
+var familyname = "Jose"
+if(familyname == "Jose"){
+    console.log("Hi! I'm a multi line comment")
+    console.log("Hi my family name is " + familyname)
+}else{
+    console.log("You entered a wrong name!")
+}
+*/
+console.log("Hi! I'm a multi line comment")
