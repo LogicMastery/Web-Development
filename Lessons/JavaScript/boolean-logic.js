83bfb3ac4d515dbf4a3714a00918d7ff30013b90
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-08 18:34:12 +0800

    added boolean logic both .js and .html file

---
+++ Lessons/JavaScript/boolean-logic.js
@@ -0,0 +1,24 @@
+var name = prompt("Enter your dog's name? ")
+if(name.toLowerCase() == "haruka"){
+    var isDog = true;
+}else{
+    var isDog = false;
+}
+
+/* Some other logic in here based on
+the variable isDog */
+if(isDog) {
+    alert("Your a big ass dog! ")
+}else{
+    alert("You motherfucker your not a dog!!!")
+}
+
+if(isDog == false) {
+    alert("Your not a dog!!!")
+}
+// if isDog is false
+if(!isDog){}
+
+if(isDog != true){
+    // this is the false block to execute
+}
