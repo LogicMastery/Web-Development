30aa39a7ce2004b3062076ce95546f9327e58f4f
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-08 22:23:32 +0800

    adding marking-lists-with-arrays.js and .html file

---
+++ Lessons/JavaScript/marking-lists-with-arrays.js
@@ -0,0 +1,19 @@
+var names = ["David", "Haruka", "Julianne", "Yuki"]
+console.log(names)
+
+names.push("Brother")
+console.log(names)
+
+twoItems = names.slice(0,2)
+console.log(twoItems)
+
+brother = names.pop()
+console.log(brother)
+console.log(typeof(brother))
+console.log(names)
+
+leng = names.length
+console.log(leng)
+
+var num = [25, 50, 90, 100]
+console.log(num)
