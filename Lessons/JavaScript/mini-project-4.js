66a55b5a4928b3703fffa04ff8a9f9935d92894e
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-12 14:37:10 +0800

    updated the mini-project-4.js adding a comments in the first line of my code about the understanding of the code

--- Lessons/JavaScript/mini-project-4.js
+++ Lessons/JavaScript/mini-project-4.js
@@ -1,3 +1,8 @@
+/*This code defines a function TipCalc that calculates a total bill amount including a tip. It takes three parameters: TotalAmount (the bill amount), 
+tip (the tip percentage, defaulting to 10%), and returnTipOnly (a flag indicating whether only the tip amount should be returned, defaulting to false).
+
+The function calculates the tip amount, adds it to the total bill, and returns either the updated total bill amount or just the tip amount based on the returnTipOnly flag.
+The code then demonstrates calling this function with different arguments to calculate the total bill amount or just the tip amount, and logs the results to the console. */
 function TipCalc(TotalAmount, tip=10, returnTipOnly=false){
     tip = tip / 100; // 10 --> 0.10
     console.log("tip as a % is ", tip)
