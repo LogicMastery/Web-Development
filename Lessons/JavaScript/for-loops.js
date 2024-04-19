2e60dcad16c84ea5081d46fcf93191f828f98648
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-19 22:09:29 +0800

    added for loops .html and .js file

---
+++ Lessons/JavaScript/for-loops.js
@@ -0,0 +1,21 @@
+var names = ["David", "Haruka", "Kirito", "Yuki", "Asuna"]
+
+for(var i = 0; i < names.length; i++){
+    var currentName = names[i]
+    console.log(i, currentName)
+}
+var words = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, obcaecati autem, non voluptate provident beatae soluta blanditiis quod tempore iure reprehenderit, in nesciunt earum corporis odit hic quis et quisquam ex id quibusdam velit distinctio voluptas! Aut expedita harum quis molestiae sit, cum omnis laborum dignissimos repellat non earum culpa.".split(" ")
+
+for (var i = 0; i < words.length; i++){
+    var currentEmail = words[i].toLowerCase() + "@gmail.com"
+
+    console.log(i, currentEmail)
+    // if(i % 2 == 0){
+    //     continue;
+    // }
+
+    if(i == 10){
+        break;
+    }
+}
+
