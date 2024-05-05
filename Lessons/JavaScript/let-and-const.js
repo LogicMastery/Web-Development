2d2018812b1a226700175ac6b3a847a75e0e66f0
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-05-05 17:33:23 +0800

    Added let and const both .html and .js file lessons

---
+++ Lessons/JavaScript/let-and-const.js
@@ -0,0 +1,23 @@
+var something = "Write something"
+
+if(something == something){
+    console.log(something)
+    var something2 = "Do you see me?"
+}
+
+console.log(something2)
+
+    // Let - it can only be seen inside the curly braces { }
+    if(something2 == something2){
+        let WriteAnything = "I'm right over here"
+        console.log(WriteAnything)
+    }
+
+    // console.log(WriteAnything)
+
+    // Const - cannot be overwrite, and it can only be seen inside the curly braces { }
+    if(something2 == something2){
+        const OverWriteMe = "I'm right over here"
+        // OverWriteMe = "Can I be changed?"
+        console.log(OverWriteMe)
+    }
