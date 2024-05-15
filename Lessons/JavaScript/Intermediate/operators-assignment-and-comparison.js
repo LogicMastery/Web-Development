30a675860314bfcb9377bfa40d604418801c5189
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-05-15 15:31:53 +0800

    added a lesson file in intermediate folder

--- Lessons/JavaScript/Intermediate/operators-assignment-and-comparison.js
+++ Lessons/JavaScript/Intermediate/operators-assignment-and-comparison.js
@@ -5,7 +5,11 @@ const surname = "Simpson";
 let number1 = 15;
 let number2 = 5;
 
-// document.getElementById('title').innerText =
+// let newNumber = number1 + number2
+number1 -+ number2
+document.getElementById('title').innerText = number1
+// document.getElementById('title').innerText = forename;
+// document.getElementById('title').innerText += ' ' + surname;
 
 // COMPARISON OPERATORS
 // ==, !=, ===, !==, >, >=, <, <=
