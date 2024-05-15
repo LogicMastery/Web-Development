3de78289c1c7df0065686d4b16c4f0ee864d9ec7
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-05-15 16:01:02 +0800

    added lesson file for intermediate level

---
+++ Lessons/JavaScript/Intermediate/operators-logical-and-arithmetic.js
@@ -0,0 +1,12 @@
+    // ARITHMETIC OPERATORS
+    // +, -, *, /, %, ++, --
+    const forename = 'Chris'
+    const surname = 'Dixon'
+    let number1 = 16
+    let number2 = 5
+
+    number1 += number2 // result = 20
+    // document.getElementById('title').innerText =
+
+    // LOGICAL OPERATORS
+    // &&, ||, !
