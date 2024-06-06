66a83ddfa207449ad15544b76996913f793935b2
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-06-06 19:23:00 +0800

    added logical operators lesson file

--- Lessons/JavaScript/Intermediate/operators-logical-and-arithmetic.js
+++ Lessons/JavaScript/Intermediate/operators-logical-and-arithmetic.js
@@ -2,11 +2,21 @@
     // +, -, *, /, %, ++, --
     const forename = 'Chris'
     const surname = 'Dixon'
-    let number1 = 16
+    let number1 = 15
     let number2 = 5
 
-    number1 += number2 // result = 20
-    // document.getElementById('title').innerText =
+    // let result = number1 * number2 // result = 20
+    // result++;
+    // result++;
+
+    // let result = number1 * number2 // result = 20
+    // result--;
+    // result--;
+
+    // let result = number1 === 15 && number2 === 4;
+    let result = number1 === 15 || number2 === 4;
+    // document.getElementById('title').innerText = result;
+    document.getElementById('title').innerText = !result;
 
     // LOGICAL OPERATORS
     // &&, ||, !
