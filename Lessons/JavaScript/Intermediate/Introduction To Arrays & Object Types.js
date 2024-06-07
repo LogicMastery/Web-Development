9aac544748980e9bfdf4852ca1da5eb76cbd5d1f
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-06-07 16:34:35 +0800

    add introduction to arrays lesson file

---
+++ Lessons/JavaScript/Intermediate/Introduction To Arrays & Object Types.js
@@ -0,0 +1,4 @@
+let pizza = ['dough', 'cheese', 'sauce', 'pepperoni']
+let HasPepperoni = pizza.includes('pepperoni')
+// document.getElementById('title').innerText = `This pizza has ${pizza.length} ingredients`;
+document.getElementById('title').innerText = HasPepperoni;
